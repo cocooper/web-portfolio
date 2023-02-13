3922ba9957fef14187b8149a05a4271e459b63bb
cocooper <cole.cooperc@gmail.com>
2023-02-13 15:52:02 -0700

    website update
    coopercdesign initial update

--- js/main.js
+++ js/main.js
@@ -2,4 +2,28 @@ const clickButton=document.querySelector('.toggle-btn');
 
 clickButton.addEventListener('click',() => {
 	document.querySelector('nav').classList.toggle('show-nav');
+});
+
+const menuButton=document.querySelector('.menu-link-one');
+
+menuButton.addEventListener('click',() => {
+	document.querySelector('nav').classList.toggle('show-nav');
+});
+
+const menuButton2=document.querySelector('.menu-link-two');
+
+menuButton2.addEventListener('click',() => {
+	document.querySelector('nav').classList.toggle('show-nav');
+});
+
+const menuButton3=document.querySelector('.menu-link-three');
+
+menuButton3.addEventListener('click',() => {
+	document.querySelector('nav').classList.toggle('show-nav');
+});
+
+const menuButton4=document.querySelector('.menu-link-four');
+
+menuButton4.addEventListener('click',() => {
+	document.querySelector('nav').classList.toggle('show-nav');
 })
